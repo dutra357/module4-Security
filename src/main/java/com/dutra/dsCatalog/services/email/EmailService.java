ab87334e52c7592a1e8f867c934e79e7abb34d69
David Alves Dutra <dadutra@hotmail.com>
2025-01-18 09:37:41 -0300

    EmailService implemented.

---
+++ src/main/java/com/dutra/dsCatalog/services/email/EmailService.java
@@ -0,0 +1,35 @@
+package com.dutra.dsCatalog.services.email;
+
+import com.dutra.dsCatalog.services.exceptions.EmailException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Value("spring.mail.username")
+    private String emailForm;
+
+    private final JavaMailSender mailSender;
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setFrom(emailForm);
+            message.setTo(to);
+            message.setText(body);
+
+            mailSender.send(message);
+
+        } catch (MailException exception) {
+            throw new EmailException("Failed to send email.");
+        }
+    }
+}
---
+++ src/main/java/com/dutra/dsCatalog/services/exceptions/EmailException.java
@@ -0,0 +1,7 @@
+package com.dutra.dsCatalog.services.exceptions;
+
+public class EmailException extends RuntimeException{
+    public EmailException(String message) {
+        super(message);
+    }
+}
